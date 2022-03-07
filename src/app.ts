4d4cc1c63bde29adbd1d415d9410e5fcee184c70
vgihan <jum654@naver.com>
2022-03-07 22:11:16 +0900

    fix: cors 모듈 삭제

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,10 @@
 import bodyParser from "body-parser";
 import express from "express";
 import postRouter from "./resources/post/post.router";
-import cors from "cors";
 
 const app = express();
 
 app.use(bodyParser());
-app.use(
-  cors({
-    origin: "http://localhost:8000",
-  })
-);
 
 app.use("/post", postRouter);
 
