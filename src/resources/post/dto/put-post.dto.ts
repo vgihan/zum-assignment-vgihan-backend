bde138f6ced5dc86f0ab5fd35ae58a3f56c35577
vgihan <jum654@naver.com>
2022-03-07 22:10:24 +0900

    fix: add constructor in dto class

--- src/resources/post/dto/put-post.dto.ts
+++ src/resources/post/dto/put-post.dto.ts
@@ -1,6 +1,14 @@
+import QueryString from "qs";
+
 export class PutPostDto {
   id!: number;
   title!: string;
   content!: string;
   writer!: string;
+  constructor(query: QueryString.ParsedQs) {
+    this.id = parseInt(query.id as string);
+    this.title = query.title as string;
+    this.content = query.content as string;
+    this.writer = query.writer as string;
+  }
 }
--- src/resources/post/dto/set-post.dto.ts
+++ src/resources/post/dto/set-post.dto.ts
@@ -1,5 +1,12 @@
+import QueryString from "qs";
+
 export class SetPostDto {
   title!: string;
   content!: string;
   writer!: string;
+  constructor(query: QueryString.ParsedQs) {
+    this.title = query.title as string;
+    this.content = query.content as string;
+    this.writer = query.writer as string;
+  }
 }
